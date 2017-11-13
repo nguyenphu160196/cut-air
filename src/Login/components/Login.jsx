fdc9188f0c4e88cd3fa2678f80afd7dacf950b1f
ThienPhu <nguyenphu160196@gmail.com>
2017-11-13 10:42:53 +0700

    update login

--- src/Login/components/Login.jsx
+++ src/Login/components/Login.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import axios from 'axios'
 import { Link } from 'react-router-dom'
 import './login.css'
 import Dialog from 'material-ui/Dialog';
@@ -8,42 +7,22 @@ import FlatButton from 'material-ui/FlatButton';
 class Login extends React.Component {
 	constructor(props) {
 		super(props);
-		this.handleSubmit = this.handleSubmit.bind(this);
-		this.handleClose = this.handleClose.bind(this);
-		this.state={display:'none', dialog:false, message:''}
 	}
 
-	handleClose = () => {
-		this.setState({dialog: false});
-	};
-
-	handleSubmit(event) {
-		event.preventDefault();
-		var data_login = {
-			email: this.loginEmail.value,
-			password: this.loginPassword.value
-		}
-		axios.post('/api/authenticate',data_login)
-		.then(function (response) {
-			var res = response.data;
-			if(res.success == true){
-				localStorage.setItem("access_token", res.token);
-				localStorage.user = JSON.stringify(res.user);
-				location.href = '/home';
-			}else{
-				this.setState({dialog:true, message: 'The email or password is incorrect!'});
-			}
-		})
-		.catch(function (error){
-		})
+	componentDidMount() {
+		window.addEventListener('scroll', this.props.handleScroll);
 	}
+	componentWillUnmount() {
+		window.removeEventListener('scroll', this.props.handleScroll);
+	}
+
 
 	render() {
 		const actions = [
 			<FlatButton
 			  label="OK"
 			  primary={true}
-			  onClick={this.handleClose}
+			  onClick={this.props.closeDialog}
 			/>
 		];
 		return (
@@ -51,9 +30,12 @@ class Login extends React.Component {
 				<div className='icon2x'></div>
 				<h1>"Cut Air"</h1>
 				<p>Đăng nhập để bắt đầu.</p>
-				<form onSubmit={this.handleSubmit}>
-					<input type='email' ref={(input) => {this.loginEmail = input;}} name='email' placeholder='Email' required/>
-					<input type='password' ref={(input) => {this.loginPassword = input;}} name='password' placeholder='Password' required/>
+				<form onSubmit={(event)=>{
+					event.preventDefault();
+					this.props.handleLogin({email: this.email.value, password: this.password.value})
+				}}>
+					<input type='email' ref={(input) => {this.email = input;}} name='email' placeholder='Email' required/>
+					<input type='password' ref={(input) => {this.password = input;}} name='password' placeholder='Password' required/>
 					<button type='submit'>Đăng nhập</button>
 					<div className='remember-me'>
 						<input type="checkbox" id="remember-me" />
@@ -63,10 +45,11 @@ class Login extends React.Component {
 				<Dialog
 					actions={actions}
 					modal={false}
-					open={this.state.dialog}
-					onRequestClose={this.handleClose}
+					open={this.props.dialog}
+					onRequestClose={this.props.closeDialog}
+					contentStyle={{width: '40%'}}
 				>
-					{this.state.message}
+					{this.props.message}
 				</Dialog>
 			</div>
 		);
