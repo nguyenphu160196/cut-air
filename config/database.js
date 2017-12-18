731544725948e53a89be50dd58ea2da5d05b9e91
ThienPhu <nguyenphu160196@gmail.com>
2017-12-18 23:17:35 +0700

    version 1.0

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   secret: 'tlcntruongngocthienphu',
   database: 'mongodb://nguyenphu160196:Phu161256@@ds161026.mlab.com:61026/tlcn_db'
+  // database: 'mongodb://localhost:27017/tlcn_db'  
 }
