d3639bb2ad6b13faaf290df09440d3ef7c4ff793
ThienPhu <nguyenphu160196@gmail.com>
2018-01-16 00:56:59 +0700

    update socket.io https

--- build/bundle.js
+++ build/bundle.js
@@ -60424,8 +60424,8 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var socket = (0, _socket2.default)('https://localhost:9090');
-// const socket = io('https://cut-air.herokuapp.com/');
+// const socket = io('https://localhost:9090');
+var socket = (0, _socket2.default)('https://cut-air.herokuapp.com/');
 
 
 var rand = __webpack_require__(533);
